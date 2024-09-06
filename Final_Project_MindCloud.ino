a45b97b9b71ee840b50f0be9c302e4ba4a64f97c
Youssef Omar the first <es-youssef.moh.ah2026@alexu.edu.eg>
2024-09-06 15:48:29 +0300

    added keypad function and password class

--- Final_Project_MindCloud.ino
+++ Final_Project_MindCloud.ino
@@ -1,12 +1,10 @@
 ////// LIBRARIES ///////////////////////
-#include <Keypad.h>
 #include <Servo.h>
-
+#include <string.h>
 /////////////////////// PINS ///////////////////////
 //keypad 
-//NOTE: if we will use SPI commu use PORTD, if we will use UART use PORTB
-byte rowPins[4] = {PIN_PD0, PIN_PD1, PIN_PD2, PIN_PD3}; 
-byte colPins[4] = {PIN_PD4, PIN_PD5, PIN_PD6, PIN_PD7}; 
+int KEYPAD_PIN = PIN_PC0;
+int keyIN = 0; //stores the keypad input
 
 //servo
 int servo_motor = PIN_PB1;
@@ -19,27 +17,77 @@ int motor = PIN_PC5;
 int pir = PIN_PB0;
 
 //temp sensor
-int temp = PIN_PC0;
+int temp = PIN_PC1;
 
 //rgb
-int ledR = PIN_PB7;
-int ledG = PIN_PB6;
-int ledB = PIN_PB5;
+int ledRGB[3] = {PIN_PB7,PIN_PB6,PIN_PB5};
 
 /////////////////////// FUNCTIONS ///////////////////////
 //keypad buttons
-char hexaKeys[4][4] = { 
-  {'1', '2', '3', 'A'},
-  {'4', '5', '6', 'B'},
-  {'7', '8', '9', 'C'},
-  {'*', '0', '#', 'D'}
+char keys[16] =  //keypad characters
+  {'1', '2', '3', 'A',
+   '4', '5', '6', 'B',
+   '7', '8', '9', 'C',
+   '*', '0', '#', 'D'};
+
+int keysVAL [16] = { //keypad numerical values
+  423, 454, 503, 562,
+  429, 459, 507, 565,
+  451, 480, 525, 579,
+  462, 487, 531, 585};
+
+int range = 1; //tolerance
+
+String get_char(int keyIN){
+  String temp = ""; //empty string to store char
+  for (int i=0; i<16; i++){
+    //if the measured value of the pressed key is within range
+    if (keyIN >= keysVAL[i]-range && keyIN <= keysVAL[i]+range){
+      temp = keys[i];
+    }
+  }
+  return temp;
+}
+
+class Password{
+  public:
+    String ans = "";
+
+    void change_pass(){
+      ans = "";
+      String x = "";
+      do{
+        keyIN = analogRead(KEYPAD_PIN);
+        x = get_char(keyIN);
+        ans += x;
+        x = ""; //empty the value in x
+      } while (x != "#");
+      
+    }
+
+    void check_pass(){
+      String temp = "";
+      String x = "";
+      do{
+        keyIN = analogRead(KEYPAD_PIN);
+        x = get_char(keyIN);
+        temp += x;
+        x = ""; //empty the value in x
+      } while (x != "#");
+
+      if (ans == temp){
+        //right password
+      }
+      else{
+        //wrong password
+      }
+    }
 };
-//connect the keypad library to our custom library
-Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
 
 /////////////////////// MAIN ///////////////////////
 void setup(){
+  Serial.begin(9600);
   //servo
   servo.attach(servo_motor); //links servo object with our servo motor(pin)
   
@@ -53,13 +101,16 @@ void setup(){
   pinMode(temp, INPUT);
 
   //rgb
-  pinMode(ledR, OUTPUT);
-  pinMode(ledG, OUTPUT);
-  pinMode(ledB, OUTPUT);
+  for (int i=0; i<3; i++){
+    pinMode(ledRGB[i], OUTPUT);
+  }
 }
 
 void loop(){
-
+  // Print if key pressed
+  keyIN = analogRead(KEYPAD_PIN);
+  Serial.println(get_char(keyIN));
+  
 }
 
 
